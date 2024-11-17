b21c57867056ed55360e7d62ef2cd1792fb8aa35
Siggi <skbirdandsiggi10@gmail.com>
2024-11-17 13:06:37 +0000

    console logs og subsub classes

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 import { renderindex } from "./lib/sites/index.js";
 import { subSite } from "./lib/sites/subSite.js";
-import { lunch } from "./lib/components/lunch.js";
 
 
-
-console.log(window.location.search);
 
 async function render(current){
     const subcurr = current.substring(0,10);
     if (current == ""){
         renderindex();
-    } else if (subcurr != "?type=html" && subcurr != "?type=java" && subcurr != "?type=css;") {
-        lunch();
     } else {
         subSite(current);
     }
